78871e662782c2d19e8f242014daff3874513c68
Zekariyas Tesfaye <99519232+ZekariyasT@users.noreply.github.com>
2025-02-25 16:37:38 +0300

    ignoring the .env file

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,17 @@
 import express from "express";
 
+import { connectDB } from "./config/db.js";
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("server is ready");
+app.post("/products", (req, res) => {
+  const product = req.body;
 });
+
+// console.log(process.env.MONGO_URI);
 app.listen(5000, () => {
+  connectDB();
   console.log("server started at http://localhost:5000");
 });
+
+// GeeQq8oaYYcfeyH7
