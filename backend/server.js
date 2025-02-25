5541b106a1d910c87de245cb3984590d016703c1
Zekariyas Tesfaye <99519232+ZekariyasT@users.noreply.github.com>
2025-02-25 18:22:40 +0300

    add delete logic

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,17 @@ app.post("/api/products", async (req, res) => {
   }
 });
 
+app.delete("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    await Product.findByIdAndDelete(id);
+    res
+      .status(200)
+      .json({ success: true, message: "Product deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Product not found" });
+  }
+});
 // console.log(process.env.MONGO_URI);
 app.listen(5000, () => {
   connectDB();
