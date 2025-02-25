2189a33e09547788d37ce2e848ae956dd33fc5cd
Zekariyas Tesfaye <99519232+ZekariyasT@users.noreply.github.com>
2025-02-25 16:51:22 +0300

    creating the logic to save data

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,25 @@
 import express from "express";
 
 import { connectDB } from "./config/db.js";
+import Product from "./models/product.model.js";
 
 const app = express();
 
-app.post("/products", (req, res) => {
+app.post("/products", async (req, res) => {
   const product = req.body;
+  if (!product.name || !product.price || !product.image) {
+    return res
+      .status(404)
+      .json({ success: false, message: "Please provide all fields" });
+  }
+  const newProduct = new Product(product);
+  try {
+    await newProduct.save();
+    res.status(201).json({ success: true, data: newProduct });
+  } catch (error) {
+    console.error("Error in Create Product", error);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
 });
 
 // console.log(process.env.MONGO_URI);
