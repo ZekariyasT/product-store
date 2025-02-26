cd849fa5d2881263f1511dbe71f4294e1dce1467
Zekariyas Tesfaye <99519232+ZekariyasT@users.noreply.github.com>
2025-02-26 15:27:01 +0300

    use port from .env

--- backend/server.js
+++ backend/server.js
@@ -5,12 +5,13 @@ import productRoutes from "./routes/product.route.js";
 
 const app = express();
 app.use(express.json());
+const PORT = process.env.PORT || 5000;
 
 app.use("/api/products", productRoutes);
 // console.log(process.env.MONGO_URI);
-app.listen(5000, () => {
+app.listen(PORT, () => {
   connectDB();
-  console.log("server started at http://localhost:5000");
+  console.log("server started at http://localhost:" + PORT);
 });
 
 // GeeQq8oaYYcfeyH7
