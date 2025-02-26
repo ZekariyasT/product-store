a09d7194f9c7e9bd09e862705a77335b77ae2ba4
Zekariyas Tesfaye <99519232+ZekariyasT@users.noreply.github.com>
2025-02-26 14:17:21 +0300

    creating select and update logic

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,17 @@ import Product from "./models/product.model.js";
 
 const app = express();
 app.use(express.json());
+
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json({ sucess: true, data: products });
+  } catch (error) {
+    console.log("Error in fetching products", error.message);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+});
+
 app.post("/api/products", async (req, res) => {
   const product = req.body;
   if (!product.name || !product.price || !product.image) {
@@ -21,7 +32,23 @@ app.post("/api/products", async (req, res) => {
     res.status(500).json({ success: false, message: "Server Error" });
   }
 });
-
+app.put("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = req.body;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res
+      .status(404)
+      .json({ success: false, message: "Invalid Product Id" });
+  }
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(id, product, {
+      new: true,
+    });
+    res.status(200).json({ success: true, data: updatedProduct });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+});
 app.delete("/api/products/:id", async (req, res) => {
   const { id } = req.params;
   try {
@@ -30,6 +57,7 @@ app.delete("/api/products/:id", async (req, res) => {
       .status(200)
       .json({ success: true, message: "Product deleted successfully" });
   } catch (error) {
+    console.log("Error in deleting product", error.message);
     res.status(500).json({ success: false, message: "Product not found" });
   }
 });
@@ -40,3 +68,5 @@ app.listen(5000, () => {
 });
 
 // GeeQq8oaYYcfeyH7
+
+// bojUG8SOZduChI7Z
