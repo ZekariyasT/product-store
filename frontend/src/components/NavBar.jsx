8e9d0ce1a5a0bea60acc12bedc498cc991047aa6
Zekariyas Tesfaye <99519232+ZekariyasT@users.noreply.github.com>
2025-02-27 17:12:17 +0300

    Creating the navigation

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,5 +1,30 @@
+import { Container, Flex, Link, Text } from "@chakra-ui/react";
+
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <Container maxW={"1140px"} px={4}>
+      <Flex
+        h={16}
+        alignItems={"center"}
+        justifyContent={"space-between"}
+        flexDir={{
+          base: "column",
+          sm: "row",
+        }}
+      >
+        <Text
+          fontSize={{ base: 22, sm: 28 }}
+          fontWeight={"bold"}
+          textTransform={"uppercase"}
+          textAlign={"center"}
+          bgGradient={"linear(to-r,cyan.400,blue.500)"}
+          bgClip={"text"}
+        >
+          <Link to="/">Product Store 🛒</Link>
+        </Text>
+      </Flex>
+    </Container>
+  );
 };
 
 export default NavBar;
