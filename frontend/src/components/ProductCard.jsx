8492ae186067518252c46f301406289223442f1e
Zekariyas Tesfaye <99519232+ZekariyasT@users.noreply.github.com>
2025-03-02 09:27:17 +0300

    Creating box and image

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,7 +1,23 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Image } from "@chakra-ui/react";
 
 const ProductCard = () => {
-  return <Box></Box>;
+  return (
+    <Box
+      shadow="lg"
+      rounded="lg"
+      overflow="hidden"
+      transition="all 0.3s "
+      _hover={{ transform: "translateY(-5px)", shadow: "xl" }}
+    >
+      <Image
+        src={product.image}
+        alt={product.name}
+        h={48}
+        w="full"
+        objectFit="cover"
+      />
+    </Box>
+  );
 };
 
 export default ProductCard;
