f3d0be558b2bf0598efd6364905299d175af3e17
Zekariyas Tesfaye <99519232+ZekariyasT@users.noreply.github.com>
2025-03-03 21:29:06 +0300

    creating edit and delete buttons

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,4 +1,4 @@
-import { Box, Image } from "@chakra-ui/react";
+import { Box, HStack, IconButton, Image, Text } from "@chakra-ui/react";
 
 const ProductCard = () => {
   return (
@@ -16,6 +16,19 @@ const ProductCard = () => {
         w="full"
         objectFit="cover"
       />
+      <Box p={4}>
+        <Heading as="h3" size="md" mb={2}>
+          {product.name}
+        </Heading>
+        <Text fontWeight="bold" fontSize="xl" color={textColor} mb={4}>
+          ${product.price}
+        </Text>
+
+        <HStack spcing={2}>
+          <IconButton icon={<EditIcon />} colorScheme="blue" />
+          <IconButton icon={<DeleteIcon />} colorShceme="red" />
+        </HStack>
+      </Box>
     </Box>
   );
 };
